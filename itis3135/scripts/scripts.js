1bafb694101f74c8642a5b687b61c80ab2cfae07
Seth LaMay <salamay1219@gmail.com>
2022-02-16 17:29:56 -0500

    s

--- itis3135/scripts/scripts.js
+++ itis3135/scripts/scripts.js
@@ -3,6 +3,11 @@
     {
         document.getElementById("showDate").innerHTML = Date();
     }
-    
+    function askInfo()
+    {
+        usersName = prompt("What is your name ?")
+        usersFeeling = prompt("How are tou feeling ? ")
+        alert ("Hey! " + usersName + "Im glad you are feeling " + usersFeeling )
+    }
 
 
