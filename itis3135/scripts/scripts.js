03dc6a463c6888eda56b1e7dce672f75d4fb8090
Seth LaMay <salamay1219@gmail.com>
2022-02-16 19:20:38 -0500

    Stuff

--- itis3135/scripts/scripts.js
+++ itis3135/scripts/scripts.js
@@ -3,17 +3,18 @@
     {
         document.getElementById("showDate").innerHTML = Date();
     }
-    function askInfo()
+    /*function askInfo()
     {
         usersName = window.prompt("What is your name ?")
         usersFeeling = window.prompt("How are tou feeling ? ")
         alert ("Hey! " + usersName + "Im glad you are feeling " + usersFeeling )
     }
+    */
     function displayUserInfo()
     {
-        userName = document.getElemenetById("Name")
-        Feelings = documnet.getElemenetById("userFeelings")
-        alert ("Hello " + userName + " You are feeling " + Feelings + " Today.")
+       var userName = document.getElemenetById("userName");
+       var Feelings = documnet.getElemenetById("userFeelings");
+       alert ("Hello " + userName + " You are feeling " + Feelings + " Today.");
     }
 
 
