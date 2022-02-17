c48725c73e543f5db2a1bbaa10dc21236a9aae46
Seth LaMay <salamay1219@gmail.com>
2022-02-16 21:51:25 -0500

    STUFF CHANGED

--- itis3135/scripts/scripts.js
+++ itis3135/scripts/scripts.js
@@ -1,20 +1,46 @@
-
-    function displayDate()
-    {
+    function displayDate() {
         document.getElementById("showDate").innerHTML = Date();
     }
-    /*function askInfo()
-    {
-        usersName = window.prompt("What is your name ?")
-        usersFeeling = window.prompt("How are tou feeling ? ")
-        alert ("Hey! " + usersName + "Im glad you are feeling " + usersFeeling )
+    
+    function displayUserInfo() {
+    
+        var userName = document.getElementById("userName").value;
+        console.log(userName);
+        var feelings = document.getElementById("userFeelings").value;
+        console.log(feelings);
+        alert("Hello " + userName + ". You are feeling " + feelings + ".");
     }
-    */
-    function displayUserInfo()
-    {
-       var userName = document.getElemenetById("userName");
-       var Feelings = documnet.getElemenetById("userFeelings");
-       alert ("Hello " + userName + " You are feeling " + Feelings + " Today.");
+
+    function calculateTax() {
+        var num1 = prompt("Please enter your bill total.");
+        var num2 = parseInt(num1);
+        var num3 = .06;
+        var num4 = (num2 * num3);
+        let taxAmount = (num4);
+        let totalAmount = (num2 + taxAmount);
+        alert("Your tax amount is: $" + taxAmount.toFixed(2) + " and your total amount is: $" + totalAmount.toFixed(2));
     }
+
+    function giveCompliment(){
+        alert("Your hair looks neat champ.")
+    }
+    function giveInsult(){
+        alert("Your hair looks bad.")
+    }
+    function sumTwoNum(){
+        var num1 = prompt("Please input a number...");
+        var num2 = prompt("Please input a second number...");
+        var num3 = parseFloat(num1) + parseFloat(num2);
+        alert("The sum of your two numbers is: " + num3);
+    }
+    function giveHonestTruth(){
+        alert("Life is meaningless so live it up now.....")
+    }
+    
+
+    
+
+
+
 
 
